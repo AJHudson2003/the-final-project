3e1a8226c8f9fbe39894703b212a4352066b1eec
AJHudson <02andrewjames.hudson@gulllakecs.org>
2019-03-11 09:37:17 -0400

    add a second question to the servey

--- final project.py/the-final-project.py
+++ final project.py/the-final-project.py
@@ -4,7 +4,7 @@
 welcome = input('Welcome to the Questions game!')
 def welcome():
     print("")
-print('')
+
 print('question number one')
 print('')
 name = input("What is your name: ")
@@ -12,3 +12,17 @@ def greeting():
     print(" hi there " + name + "!")
     print("nice to meet you")
 greeting()
+
+print('')
+print('questions two!')
+print('')
+total = 0
+how_many_tests = int(input("How many test would you like to average today:  "))
+print("")
+for i in range(how_many_tests):
+    score = float(input("enter score for tests: "))
+    total = total + score
+average = total / how_many_tests
+print("")
+print("average: " + str(round(average, 2)))
+print(type(average)
